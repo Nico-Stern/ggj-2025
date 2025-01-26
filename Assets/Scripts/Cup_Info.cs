f3e1de0957d803af8c21b5a06b741324b0743864
Nico-Stern <nstern2002@gmail.com>
2025-01-26 09:24:03 +0100

    Revert "test"
    This reverts commit 3a7edddf36f30fb6de520d1878879700eee9ad91.

--- Assets/Scripts/Cup_Info.cs
+++ Assets/Scripts/Cup_Info.cs
@@ -54,16 +54,8 @@ public class Cup_Info : MonoBehaviour
             transform.SetParent(collision.transform);
             GetComponent<Rigidbody>().isKinematic = true;
             GetComponent<Collider>().isTrigger = true;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
             collision.gameObject.GetComponent<ControllerPlayer>().HasPlayerTheCup(true);
-=======
-            collision.gameObject.GetComponent<ControllerPlayer>().PlayerHasCup(true);
->>>>>>> Stashed changes
-=======
-            collision.gameObject.GetComponent<ControllerPlayer>().PlayerHasCup(true);
->>>>>>> Stashed changes
         }
     }
 
